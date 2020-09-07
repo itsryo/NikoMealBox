54512a66dd86c015ad142abd68d282484c4f88da
JIn Cheng Wei <waynechin910256@gmail.com>
2020-09-07 17:03:58 +0800

    Create: 購物車(Order)相關類別

---
+++ NikoMealBox/DataAccess/Repository/OrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using NikoMealBox.Models.DataTable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NikoMealBox.DataAccess.Repository
+{
+    public class OrderRepository:GenericRepository<Orders>
+    {
+    }
+}
---
+++ NikoMealBox/ViewModels/OrderViewModels.cs
@@ -0,0 +1,16 @@
+﻿using NikoMealBox.Models.DataTable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NikoMealBox.ViewModels
+{
+    public class OrderViewModels
+    {
+        public class Index
+        {
+            public List<OrderDetails> OrderDetail { get; set; }
+        }
+    }
+}
