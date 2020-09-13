c09dc468f071aedbc7fb2bf9a72d45ccf53ef168
Waynelens <waynechin910256@gmail.com>
2020-09-14 00:14:30 +0800

    Clear some template code

--- NikoMealBox/Controllers/HomeController.cs
+++ NikoMealBox/Controllers/HomeController.cs
@@ -15,19 +15,10 @@ namespace NikoMealBox.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
-        public ActionResult Test()
         {
             return View();
         }
