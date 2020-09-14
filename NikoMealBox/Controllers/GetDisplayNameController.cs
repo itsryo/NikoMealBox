054996d098a109db55cc19e5cad229590bb27fdd
Waynelens <waynechin910256@gmail.com>
2020-09-14 13:06:59 +0800

    Controller combine

---
+++ NikoMealBox/Controllers/GetDisplayNameController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NikoMealBox.Controllers
+{
+    public class GetDisplayNameController : Controller
+    {
+        // GET: GetDisplayName
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
