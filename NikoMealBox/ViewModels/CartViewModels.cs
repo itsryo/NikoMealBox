75762ad8225bc4e423c70edf5e4ddb6f3eba270d
ping0924 <yuwanping0924@gmail.com>
2020-09-14 18:20:37 +0800

    加入購物車同商品數量增加

--- NikoMealBox/ViewModels/CartViewModels.cs
+++ NikoMealBox/ViewModels/CartViewModels.cs
@@ -66,7 +66,7 @@ namespace NikoMealBox.ViewModels
             }
             else
             {   //存在購物車內，則將商品數量累加
-                findItem.Count += 1;
+                findItem.Count += quantity;
             }
             return true;
         }
