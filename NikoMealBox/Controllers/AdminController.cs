0043dd22fd53875f44d1b02afd83f13e3f85ea9b
ping0924 <yuwanping0924@gmail.com>
2020-09-15 18:02:53 +0800

    後台撈商品資料

--- NikoMealBox/Controllers/AdminController.cs
+++ NikoMealBox/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NikoMealBox.DataAccess.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,21 @@ namespace NikoMealBox.Controllers
 {
     public class AdminController : Controller
     {
+        private ProductRepository _repository;
+        public AdminController()
+        {
+
+            _repository = new ProductRepository();
+        }
         // GET: Addmin
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult Admin_SelectProducts()
+        {
+            var products = _repository.Select();
+            return Json(products, JsonRequestBehavior.AllowGet);
         }
     }
 }
