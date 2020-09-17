7e36e00d94aa2fbb2c41553a55108ef8d8301895
昆泉 王 <zo334411@gmail.com>
2020-09-17 21:58:18 +0800

    暫時的Chart

---
+++ NikoMealBox/Controllers/WebAPI/ChartController.cs
@@ -0,0 +1,44 @@
+﻿using NikoMealBox.DataAccess.Repository;
+using NikoMealBox.Models;
+using NikoMealBox.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NikoMealBox.Controllers.WebAPI
+{
+    [RoutePrefix("api/[Controller]/[action]")]
+    public class ChartController : ApiController
+    {
+
+        private ApplicationDbContext db;
+        private ProductRepository _repository;
+
+        public ChartController()
+        {
+            db = new ApplicationDbContext();
+            _repository = new ProductRepository();
+        }
+
+
+        [AcceptVerbs("GET", "POST")]
+        public List<ChartViewModels.ProductsResult> Chart()
+        {
+            var chartVM = new List<ChartViewModels.ProductsResult>();
+            var source = _repository.GetAll();
+            foreach (var item in source)
+            {
+                chartVM.Add(new ChartViewModels.ProductsResult
+                {
+                    ProductName = item.ProductName,
+                    UnitStock = item.UnitsInStock
+                });
+            }
+
+            return chartVM;
+        }
+    }
+}
---
+++ NikoMealBox/ViewModels/ChartViewModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NikoMealBox.ViewModels
+{
+    public class ChartViewModels
+    {
+
+        public class ProductsResult
+        {
+            public string ProductName { get; set; }
+            public int UnitStock { get; set; }
+
+        }
+
+    }
+   
+}
