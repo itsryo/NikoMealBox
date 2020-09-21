db92c43ff6a7384e0ff7e98b6d8c24be25bff9b4
Waynelens <waynechin910256@gmail.com>
2020-09-21 12:37:24 +0800

    Order Create

--- NikoMealBox/Controllers/CartAllController.cs
+++ NikoMealBox/Controllers/CartAllController.cs
@@ -16,6 +16,42 @@ namespace NikoMealBox.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult CreateOrder(string username,string sex,string phone,string mail,string city,string district,string detailaddress,string remark,string deliverydate,string deliverytime,string payment)
+        {
+            return RedirectToAction("OrderList", new 
+                {   username = username, 
+                    sex = sex, 
+                    phone = phone,
+                    mail = mail,
+                    city = city,
+                    district = district,
+                    detailaddress = detailaddress,
+                    remark = remark,
+                    deliverydate = deliverydate,
+                    deliverytime = deliverytime,
+                    payment = payment
+                }
+            );
+        }
+
+        public ActionResult OrderList(string username,string sex,int phone, string mail,string city,string district,string detailaddress,string remark,string deliverydate,string deliverytime,string payment)
+        {
+            ViewData["username"] = username;
+            ViewData["sex"] = sex;
+            ViewData["phone"] = phone;
+            ViewData["mail"] = mail;
+            ViewData["city"] = city;
+            ViewData["district"] = district;
+            ViewData["detailaddress"] = detailaddress;
+            ViewData["remark"] = remark;
+            ViewData["deliverydate"] = deliverydate;
+            ViewData["deliverytime"] = deliverytime;
+            ViewData["payment"] = payment;
+
+            return View();
+        }
+
         public ActionResult RemoveFromCart(int id)
         {
             var currentCart = CartRepository.GetCurrentCart();
