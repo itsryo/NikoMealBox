8c4b07b60f749d14971fcb569c56a5445a68b6a3
JIn Cheng Wei <waynechin910256@gmail.com>
2020-09-24 14:12:01 +0800

    訂單寫入DB，OrderDetail可以記錄同一筆訂單的所有項目

--- NikoMealBox/Controllers/CartAllController.cs
+++ NikoMealBox/Controllers/CartAllController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using NikoMealBox.DataAccess.Repository;
 using NikoMealBox.Models;
@@ -7,12 +8,15 @@ using NikoMealBox.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Core.Metadata.Edm;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography.Xml;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Services.Protocols;
+using WebGrease;
 
 namespace NikoMealBox.Controllers
 {
@@ -72,17 +76,47 @@ namespace NikoMealBox.Controllers
             return View(OrderForm);
         }
 
-        public ActionResult CreateOrder(string city,string region,string address)
+        public ActionResult CreateOrder(OrderViewModels OrderForm)
         {
-            //Orders order = new Orders();
-            //order.PickUpCity = city;
-            //order.PickUpRegion = region;
-            //order.PickUpAddress = address;
-            //DbContext dbContext = new ApplicationDbContext();
-            //DbSet<Orders> Set = dbContext.Set<Orders>();
-            //Set.Add(order);
-            //dbContext.SaveChanges();
+            var db = new ApplicationDbContext();
+            var set = db.Set<Orders>();
+            var cart = CartRepository.GetCurrentCart();
+            List<OrderDetails> orderDetailList = new List<OrderDetails>();
+            var Order2 = new Orders
+            {
+                //Id自產
+                CreateUser = OrderForm.CreateUser,
+                CreateTime = DateTime.Now,
+                GetProductDate = OrderForm.GetProductDate,
+                // 當初設定不為null，避免datetime2 to datetime問題，先這樣
+                FinishDate = new DateTime(9999,1,1),
+                PickUpCity = OrderForm.PickUpCity,
+                PickUpRegion = OrderForm.PickUpRegion,
+                PickUpAddress = OrderForm.PickUpAddress,
+                ContactPhone = OrderForm.ContactPhone,
+                ContactMail = OrderForm.ContactMail,
+                Remark = OrderForm.Remark,
+                Payment = OrderForm.Payment,
+                OrderDetails = orderDetailList,
+                //userRefId 等一下再用
+            };
+            foreach (var item in cart)
+            {
+                var orderDetail = new OrderDetails()
+                {
+                    Quantity = item.Count,
+                    CreateUser = OrderForm.CreateUser,
+                    CreateTime = DateTime.Now,
+                    OrdersId = Order2.Id,
+                    Orders = Order2,
+                    ProductsId = item.Id,
+                    Products = new ProductRepository().SelectAllProducts().Where(x =>x.Id == item.Id).FirstOrDefault(),
+                };
+                orderDetailList.Add(orderDetail);
+            }
 
+            set.Add(Order2);
+            db.SaveChanges();
             return View();
         }
 
