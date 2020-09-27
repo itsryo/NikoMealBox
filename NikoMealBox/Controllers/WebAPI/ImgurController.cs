3ff34f63d42bc5283256aac5078fa46bff8ee07a
Waynelens <waynechin910256@gmail.com>
2020-09-28 00:40:29 +0800

    ImgurController的上傳部分postman測試OK，但ajax請求傳入一直為null

--- NikoMealBox/Controllers/WebAPI/ImgurController.cs
+++ NikoMealBox/Controllers/WebAPI/ImgurController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Core.Metadata.Edm;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -9,9 +10,13 @@ using Imgur;
 using Imgur.API.Authentication;
 using Imgur.API.Endpoints;
 using Imgur.API.Models;
+using Microsoft.Ajax.Utilities;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace NikoMealBox.Controllers.WebAPI
 {
+    [RoutePrefix("api/[Controller]/[action]")]
     public class ImgurController : ApiController
     {
         // GET: api/Imgur
@@ -23,12 +28,22 @@ namespace NikoMealBox.Controllers.WebAPI
         // GET: api/Imgur/5
         public string Get(int id)
         {
-            return "value";
+            return id.ToString();
         }
 
         // POST: api/Imgur
-        public void Post([FromBody]string value)
+        public HttpResponseMessage Post([FromBody] string value)
         {
+            //範例
+            JObject request = JObject.Parse(value);
+            string name = request["name"].ToString();
+            string gender = request["gender"].ToString();
+            var result = new
+            {
+                newname = name,
+                newgender = gender,
+            };
+            return Request.CreateResponse(HttpStatusCode.OK, result , GlobalConfiguration.Configuration.Formatters.JsonFormatter);
         }
 
         // PUT: api/Imgur/5
@@ -41,8 +56,9 @@ namespace NikoMealBox.Controllers.WebAPI
         {
         }
 
-        // Test imgur upload
-        public async System.Threading.Tasks.Task<string> UploadAsync()
+        // Upload: api/Imgur/UploadAsync
+        [HttpPost]
+        public async System.Threading.Tasks.Task<HttpResponseMessage> UploadAsync([FromBody]string base64json)
         {
             var apiclient = new ApiClient("1b1ed0a9fc10214", "36ebd8d5c71376376eebc39a6246ca429c297f50");
             var httpclient = new HttpClient();
@@ -59,11 +75,17 @@ namespace NikoMealBox.Controllers.WebAPI
             apiclient.SetOAuth2Token(token);
             var imageEndpoint = new ImageEndpoint(apiclient, httpclient);
 
-            var filePath = "C:\\Users\\wayne\\Downloads\\ImgurAPI\\Imgur\\Picture\\abc.png";
-            var fileStream = File.OpenRead(filePath);
-            var imageUpload = await imageEndpoint.UploadImageAsync(fileStream);
+            //反序
+            JObject request = JObject.Parse(base64json);
+            byte[] data = System.Convert.FromBase64String(request["base64"].ToString());
+            MemoryStream ms = new MemoryStream(data);
+            var imageUpload = await imageEndpoint.UploadImageAsync(ms);
+            var response = new
+            {
+                imageUrl = imageUpload.Link,
+            };
 
-            return imageUpload.Link;
+            return Request.CreateResponse(HttpStatusCode.OK, response, GlobalConfiguration.Configuration.Formatters.JsonFormatter);
         }
     }
 }
